22c5aa73cfb2193a156699a26af144624c8ef3e6
luoshuitaotao <jcui@unomaha.edu>
2020-08-21 13:37:36 -0500

    add one to one relationship

--- ManyToOne/admin.py
+++ ManyToOne/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Article, Reporter
+from .models import Article, Reporter, Place, Restaurant
 
 admin.site.register(Article)
 admin.site.register(Reporter)
+admin.site.register(Place)
+admin.site.register(Restaurant)
--- ManyToOne/models.py
+++ ManyToOne/models.py
@@ -21,4 +21,17 @@ class Article(models.Model):
         ordering = ('headline',)
 
 
+class Place(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(Place, on_delete=models.CASCADE, primary_key=True)
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.place.name
